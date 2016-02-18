403b640ce365c047f0bf920acdfb56b7e66cdf0f
Matthew Deogracais <nolan0597@yahoo.com>
2016-02-18 19:11:47 +0800

    Reupload

---
+++ Test Project/Purchase Order Maintenance Application/Queries.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MySql.Data.MySqlClient;
+using System.Configuration;
+using System.Data;
+
+namespace Purchase_Order_Maintenance_Application
+{
+    class Queries
+    {
+        public Queries()
+        {
+
+        }
+        public Boolean checkConnection()    //This function checks whether there is a connection to the mySQL database
+        {
+            try
+            {
+                string conString = ConfigurationManager.ConnectionStrings["conString"].ConnectionString;
+                MySqlConnection connection = new MySqlConnection(conString);
+                MySqlDataAdapter sda = new MySqlDataAdapter();
+                sda.SelectCommand = new MySqlCommand("SELECT * FROM employee", connection);
+                MySqlCommandBuilder cb = new MySqlCommandBuilder(sda);
+                connection.Open();
+                connection.Close();
+                return true;
+            }
+            catch(Exception ex)
+            {
+                return false;
+            }
+
+        }
+    }
+}
---
+++ Test Project/Purchase Order Maintenance Application/TestPage.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Configuration;
+using MySql.Data.MySqlClient;
+
+namespace Purchase_Order_Maintenance_Application
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void ConnectButton_Click_1(object sender, EventArgs e)
+        {
+            Queries Q = new Queries();
+
+            if (Q.checkConnection())
+                MessageBox.Show("Connected");
+            else
+                MessageBox.Show("Error Connecting");
+        }
+    }
+}
