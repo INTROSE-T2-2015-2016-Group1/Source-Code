a2fd9f096f9d7e6ce8987a72df19373527e754cd
Matthew Deogracais <nolan0597@yahoo.com>
2016-03-03 21:29:48 +0800

    Updated creation queries

--- Database/INTROSE SQL QUERIES/create_item_list.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE `item_list` ( part_number INT(10),
-company VARCHAR(20),
-description VARCHAR(20),
-price INT(10),
-unit_price VARCHAR(20),
-unit_measure VARCHAR(20),
-PRIMARY KEY (part_number)
-);
