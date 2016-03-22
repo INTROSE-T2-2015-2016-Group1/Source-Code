8d82b830062d5cb842dcad1ae9c6193804ff25c8
Paolo Nablo <juan_nablo@dlsu.edu.ph>
2016-03-22 20:17:12 +0800

    updated GUI v2

--- INTROSE_Project/WindowsFormsApplication1/sub_windows/Purchase Order/Customer/updateCPO.Designer.cs
+++ INTROSE_Project/WindowsFormsApplication1/sub_windows/Purchase Order/Customer/updateCPO.Designer.cs
@@ -29,166 +29,97 @@
         private void InitializeComponent()
         {
             this.updateCPOBtn = new System.Windows.Forms.Button();
-            this.groupBox2 = new System.Windows.Forms.GroupBox();
-            this.label4 = new System.Windows.Forms.Label();
-            this.itemNumCBox = new System.Windows.Forms.ComboBox();
-            this.label5 = new System.Windows.Forms.Label();
-            this.label6 = new System.Windows.Forms.Label();
-            this.pricePerUnitTxtBox = new System.Windows.Forms.TextBox();
-            this.label7 = new System.Windows.Forms.Label();
-            this.currencyTxtBox = new System.Windows.Forms.TextBox();
-            this.itemQtyTxtBox = new System.Windows.Forms.TextBox();
-            this.dateExpectedCBox = new System.Windows.Forms.DateTimePicker();
-            this.customerNameCBox = new System.Windows.Forms.ComboBox();
             this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.cPONumberTxtBox = new System.Windows.Forms.TextBox();
             this.label1 = new System.Windows.Forms.Label();
+            this.customerNameCBox = new System.Windows.Forms.ComboBox();
             this.dateIssuedCBox = new System.Windows.Forms.DateTimePicker();
+            this.dateExpectedCBox = new System.Windows.Forms.DateTimePicker();
             this.label2 = new System.Windows.Forms.Label();
             this.label3 = new System.Windows.Forms.Label();
-            this.groupBox2.SuspendLayout();
             this.groupBox1.SuspendLayout();
             this.SuspendLayout();
             // 
             // updateCPOBtn
             // 
-            this.updateCPOBtn.Location = new System.Drawing.Point(319, 317);
+            this.updateCPOBtn.Location = new System.Drawing.Point(159, 347);
             this.updateCPOBtn.Name = "updateCPOBtn";
             this.updateCPOBtn.Size = new System.Drawing.Size(127, 23);
             this.updateCPOBtn.TabIndex = 19;
             this.updateCPOBtn.Text = "Update Purchase Order";
             this.updateCPOBtn.UseVisualStyleBackColor = true;
             // 
-            // groupBox2
-            // 
-            this.groupBox2.Controls.Add(this.label4);
-            this.groupBox2.Controls.Add(this.itemNumCBox);
-            this.groupBox2.Controls.Add(this.label5);
-            this.groupBox2.Controls.Add(this.label6);
-            this.groupBox2.Controls.Add(this.pricePerUnitTxtBox);
-            this.groupBox2.Controls.Add(this.label7);
-            this.groupBox2.Controls.Add(this.currencyTxtBox);
-            this.groupBox2.Controls.Add(this.itemQtyTxtBox);
-            this.groupBox2.Location = new System.Drawing.Point(258, 13);
-            this.groupBox2.Name = "groupBox2";
-            this.groupBox2.Size = new System.Drawing.Size(188, 291);
-            this.groupBox2.TabIndex = 21;
-            this.groupBox2.TabStop = false;
-            this.groupBox2.Text = "Item Information";
-            // 
-            // label4
-            // 
-            this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(6, 28);
-            this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(67, 13);
-            this.label4.TabIndex = 3;
-            this.label4.Text = "Item Number";
-            // 
-            // itemNumCBox
-            // 
-            this.itemNumCBox.FormattingEnabled = true;
-            this.itemNumCBox.Location = new System.Drawing.Point(9, 45);
-            this.itemNumCBox.Name = "itemNumCBox";
-            this.itemNumCBox.Size = new System.Drawing.Size(133, 21);
-            this.itemNumCBox.TabIndex = 14;
-            // 
-            // label5
-            // 
-            this.label5.AutoSize = true;
-            this.label5.Location = new System.Drawing.Point(6, 85);
-            this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(69, 13);
-            this.label5.TabIndex = 4;
-            this.label5.Text = "Item Quantity";
-            // 
-            // label6
-            // 
-            this.label6.AutoSize = true;
-            this.label6.Location = new System.Drawing.Point(6, 152);
-            this.label6.Name = "label6";
-            this.label6.Size = new System.Drawing.Size(49, 13);
-            this.label6.TabIndex = 5;
-            this.label6.Text = "Currency";
-            // 
-            // pricePerUnitTxtBox
-            // 
-            this.pricePerUnitTxtBox.Location = new System.Drawing.Point(9, 229);
-            this.pricePerUnitTxtBox.Name = "pricePerUnitTxtBox";
-            this.pricePerUnitTxtBox.Size = new System.Drawing.Size(133, 20);
-            this.pricePerUnitTxtBox.TabIndex = 10;
-            // 
-            // label7
-            // 
-            this.label7.AutoSize = true;
-            this.label7.Location = new System.Drawing.Point(6, 213);
-            this.label7.Name = "label7";
-            this.label7.Size = new System.Drawing.Size(72, 13);
-            this.label7.TabIndex = 6;
-            this.label7.Text = "Price Per Unit";
-            // 
-            // currencyTxtBox
-            // 
-            this.currencyTxtBox.Location = new System.Drawing.Point(9, 168);
-            this.currencyTxtBox.Name = "currencyTxtBox";
-            this.currencyTxtBox.Size = new System.Drawing.Size(133, 20);
-            this.currencyTxtBox.TabIndex = 9;
-            // 
-            // itemQtyTxtBox
-            // 
-            this.itemQtyTxtBox.Location = new System.Drawing.Point(9, 102);
-            this.itemQtyTxtBox.Name = "itemQtyTxtBox";
-            this.itemQtyTxtBox.Size = new System.Drawing.Size(133, 20);
-            this.itemQtyTxtBox.TabIndex = 8;
-            // 
-            // dateExpectedCBox
-            // 
-            this.dateExpectedCBox.Location = new System.Drawing.Point(6, 169);
-            this.dateExpectedCBox.Name = "dateExpectedCBox";
-            this.dateExpectedCBox.Size = new System.Drawing.Size(186, 20);
-            this.dateExpectedCBox.TabIndex = 13;
-            // 
-            // customerNameCBox
-            // 
-            this.customerNameCBox.FormattingEnabled = true;
-            this.customerNameCBox.Location = new System.Drawing.Point(18, 58);
-            this.customerNameCBox.Name = "customerNameCBox";
-            this.customerNameCBox.Size = new System.Drawing.Size(183, 21);
-            this.customerNameCBox.TabIndex = 18;
-            // 
             // groupBox1
             // 
+            this.groupBox1.Controls.Add(this.label4);
+            this.groupBox1.Controls.Add(this.cPONumberTxtBox);
             this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.customerNameCBox);
             this.groupBox1.Controls.Add(this.dateIssuedCBox);
             this.groupBox1.Controls.Add(this.dateExpectedCBox);
             this.groupBox1.Controls.Add(this.label2);
             this.groupBox1.Controls.Add(this.label3);
             this.groupBox1.Location = new System.Drawing.Point(12, 12);
             this.groupBox1.Name = "groupBox1";
-            this.groupBox1.Size = new System.Drawing.Size(200, 292);
+            this.groupBox1.Size = new System.Drawing.Size(274, 329);
             this.groupBox1.TabIndex = 20;
             this.groupBox1.TabStop = false;
             this.groupBox1.Text = "Customer Information";
             // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(6, 31);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(121, 13);
+            this.label4.TabIndex = 15;
+            this.label4.Text = "Purchase Order Number";
+            // 
+            // cPONumberTxtBox
+            // 
+            this.cPONumberTxtBox.Location = new System.Drawing.Point(8, 47);
+            this.cPONumberTxtBox.MaxLength = 40;
+            this.cPONumberTxtBox.Name = "cPONumberTxtBox";
+            this.cPONumberTxtBox.Size = new System.Drawing.Size(260, 20);
+            this.cPONumberTxtBox.TabIndex = 14;
+            // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(3, 29);
+            this.label1.Location = new System.Drawing.Point(6, 89);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(82, 13);
             this.label1.TabIndex = 0;
             this.label1.Text = "Customer Name";
             // 
+            // customerNameCBox
+            // 
+            this.customerNameCBox.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.customerNameCBox.FormattingEnabled = true;
+            this.customerNameCBox.Location = new System.Drawing.Point(6, 105);
+            this.customerNameCBox.Name = "customerNameCBox";
+            this.customerNameCBox.Size = new System.Drawing.Size(262, 21);
+            this.customerNameCBox.TabIndex = 12;
+            // 
             // dateIssuedCBox
             // 
-            this.dateIssuedCBox.Location = new System.Drawing.Point(6, 103);
+            this.dateIssuedCBox.Location = new System.Drawing.Point(6, 163);
             this.dateIssuedCBox.Name = "dateIssuedCBox";
-            this.dateIssuedCBox.Size = new System.Drawing.Size(186, 20);
+            this.dateIssuedCBox.Size = new System.Drawing.Size(262, 20);
             this.dateIssuedCBox.TabIndex = 11;
+            // 
+            // dateExpectedCBox
+            // 
+            this.dateExpectedCBox.Location = new System.Drawing.Point(8, 229);
+            this.dateExpectedCBox.Name = "dateExpectedCBox";
+            this.dateExpectedCBox.Size = new System.Drawing.Size(260, 20);
+            this.dateExpectedCBox.TabIndex = 13;
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(3, 87);
+            this.label2.Location = new System.Drawing.Point(6, 147);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(64, 13);
             this.label2.TabIndex = 7;
@@ -197,7 +128,7 @@
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(3, 153);
+            this.label3.Location = new System.Drawing.Point(5, 213);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(119, 13);
             this.label3.TabIndex = 2;
@@ -207,15 +138,11 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(459, 352);
-            this.Controls.Add(this.updateCPOBtn);
-            this.Controls.Add(this.groupBox2);
-            this.Controls.Add(this.customerNameCBox);
+            this.ClientSize = new System.Drawing.Size(298, 382);
             this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.updateCPOBtn);
             this.Name = "updateCPO";
             this.Text = "Update Customer Purchase Order";
-            this.groupBox2.ResumeLayout(false);
-            this.groupBox2.PerformLayout();
             this.groupBox1.ResumeLayout(false);
             this.groupBox1.PerformLayout();
             this.ResumeLayout(false);
@@ -225,20 +152,13 @@
         #endregion
 
         private System.Windows.Forms.Button updateCPOBtn;
-        private System.Windows.Forms.GroupBox groupBox2;
-        private System.Windows.Forms.Label label4;
-        private System.Windows.Forms.ComboBox itemNumCBox;
-        private System.Windows.Forms.Label label5;
-        private System.Windows.Forms.Label label6;
-        private System.Windows.Forms.TextBox pricePerUnitTxtBox;
-        private System.Windows.Forms.Label label7;
-        private System.Windows.Forms.TextBox currencyTxtBox;
-        private System.Windows.Forms.TextBox itemQtyTxtBox;
-        private System.Windows.Forms.DateTimePicker dateExpectedCBox;
-        private System.Windows.Forms.ComboBox customerNameCBox;
         private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox cPONumberTxtBox;
         private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ComboBox customerNameCBox;
         private System.Windows.Forms.DateTimePicker dateIssuedCBox;
+        private System.Windows.Forms.DateTimePicker dateExpectedCBox;
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Label label3;
     }
