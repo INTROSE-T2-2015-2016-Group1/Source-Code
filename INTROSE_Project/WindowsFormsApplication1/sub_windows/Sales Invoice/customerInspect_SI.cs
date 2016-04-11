00771b998320fb2ec21c34b38c0e3e09b6d73c4a
Matthew Deogracais <nolan0597@yahoo.com>
2016-04-11 15:48:34 +0800

    Forgot to add this again

--- INTROSE_Project/WindowsFormsApplication1/sub_windows/Sales Invoice/customerInspect_SI.cs
+++ INTROSE_Project/WindowsFormsApplication1/sub_windows/Sales Invoice/customerInspect_SI.cs
@@ -13,22 +13,26 @@ namespace introse_project.sub_windows.Sales_Invoice
 {
     public partial class customerInspect_SI : Form
     {
+        #region Variables
         private static customerInspect_SI theInstance = new customerInspect_SI();
         private int customerOrderID;
         private int invoiceItemID;
+        #endregion
 
         private customerInspect_SI()
         {
             InitializeComponent();
         }
 
+        #region Event Handlers
         private void customerInspect_SI_Load(object sender, EventArgs e)
         {
             approvedQtyTxtBox_U.Text = InvoicesItemsManager.instance.getApprovedQuantity(this.customerOrderID).ToString();
             rejectedQtyTxtBox_U.Text = InvoicesItemsManager.instance.getRejectedQuantity(this.customerOrderID).ToString();
-        } 
+        }
+        #endregion
 
-
+        #region Setters
         public void setCustomerOrderID(int customerOrderID)
         {
             this.customerOrderID = customerOrderID;
@@ -38,6 +42,7 @@ namespace introse_project.sub_windows.Sales_Invoice
         {
             this.invoiceItemID = invoiceItemID;
         }
+        #endregion
 
         #region Key Press Event Handlers
         private void approvedQtyTxtBox_U_KeyPress(object sender, KeyPressEventArgs e)
@@ -57,12 +62,14 @@ namespace introse_project.sub_windows.Sales_Invoice
         }
         #endregion
 
+        #region Button Click Events
         private void updateSI_CIRBtn_Click(object sender, EventArgs e)
         {
             InvoicesItemsManager.instance.updateData(this.invoiceItemID,Convert.ToInt32(approvedQtyTxtBox_U.Text), Convert.ToInt32(rejectedQtyTxtBox_U.Text));
             CustomerOrderItemsManagercs.instance.updateFinished(this.customerOrderID);
         }
-      
+        #endregion
+       
         public static customerInspect_SI instance
         {
             get { return theInstance; }
